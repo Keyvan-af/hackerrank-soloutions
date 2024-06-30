9aa5dd8bef0e7fcdb77b43c3f28157ae909bb00c
Keyvan-af <70849668+Keyvan-af@users.noreply.github.com>
2024-06-30 17:55:17 +0330

    Practice More! :airplane:

---
+++ Python/Set.difference() Operation/solution.py
@@ -0,0 +1,8 @@
+n_E = int(input())
+L_E = input().split(" ")
+n_F = int(input())
+L_F = input().split(" ")
+L_E = [int(num) for num in L_E]
+L_F = [int(num) for num in L_F]
+
+print(len(set(L_E) - set(L_F)))
