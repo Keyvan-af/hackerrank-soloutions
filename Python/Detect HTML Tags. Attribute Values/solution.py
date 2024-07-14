c43911479aaef4cfff9eb0530f41c988abedc4ac
AliFaridCollide <kafzalnia@yahoo.com>
2024-07-14 21:44:29 +0330

    Solve python challenges

---
+++ Python/Detect HTML Tags. Attribute Values/solution.py
@@ -0,0 +1,15 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        self.print_attrs(attrs)
+
+    def print_attrs(self, attrs):
+        for item in attrs:
+            name , value = item
+            print(f'-> {name} > {value}')
+
+my_parser  = MyHTMLParser()
+html = "".join(input() for _ in range(int(input())))
+my_parser.feed(html)
